7e429cdcffb255231c349c109c699fb20b802594
Shannon Lewis <slewis74@gmail.com>
2023-01-20 12:14:39 +1000

    fixed compile issue that slipped through

--- source/tasks/Utils/pluginInformation.ts
+++ source/tasks/Utils/pluginInformation.ts
@@ -2,6 +2,6 @@ function getPluginAndVersionInformation() {
     return `plugin/${process.env.EXTENSION_VERSION}`;
 }
 
-export function getUserAgentApp(stepNoun: string, stepVerb: string, stepVersion: number): Promise<string> {
-    return `${await getPluginAndVersionInformation()} (${stepNoun};${stepVerb};v${stepVersion})`;
+export function getUserAgentApp(stepNoun: string, stepVerb: string, stepVersion: number): string {
+    return `${getPluginAndVersionInformation()} (${stepNoun};${stepVerb};v${stepVersion})`;
 }
