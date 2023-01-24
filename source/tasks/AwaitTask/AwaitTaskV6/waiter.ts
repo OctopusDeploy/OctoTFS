6e63dbc4e6fd000b4489bb37104ebd0c5f3f37c9
Ben Pearce <ben.pearce@octopus.com>
2023-01-24 14:16:58 +1000

    expanded await task output variables

--- source/tasks/AwaitTask/AwaitTaskV6/waiter.ts
+++ source/tasks/AwaitTask/AwaitTaskV6/waiter.ts
@@ -1,4 +1,10 @@
-import { Client, ClientConfiguration, Logger, ServerTaskWaiter, TaskState } from "@octopusdeploy/api-client";
+import {
+    Client,
+    ClientConfiguration,
+    Logger,
+    ServerTaskWaiter, SpaceRepository,
+    TaskState
+} from "@octopusdeploy/api-client";
 import { OctoServerConnectionDetails } from "tasks/Utils/connection";
 import { TaskWrapper } from "tasks/Utils/taskInput";
 import { getUserAgentApp } from "../../Utils/pluginInformation";
@@ -8,6 +14,7 @@ export interface DeploymentResult {
     serverTaskId: string;
     tenantName: string;
     environmentName: string;
+    successful: boolean;
 }
 
 export class Waiter {
@@ -27,7 +34,7 @@ export class Waiter {
         const waiter = new ServerTaskWaiter(client, inputParameters.space);
 
         const taskIds: string[] = [];
-        const failedTaskIds: string[] = [];
+        const deploymentResults: DeploymentResult[] = [];
         const lookup: Map<string, DeploymentResult> = new Map<string, DeploymentResult>();
         inputParameters.tasks.map((t) => {
             lookup.set(t.serverTaskId, t);
@@ -37,26 +44,60 @@ export class Waiter {
         await waiter.waitForServerTasksToComplete(taskIds, inputParameters.pollingInterval * 1000, inputParameters.timeout * 1000, (t) => {
             let context = "";
             const deploymentResult = lookup.get(t.Id);
-            if (deploymentResult?.environmentName) {
-                context = ` to environment '${deploymentResult.environmentName}'`;
-            }
-            if (deploymentResult?.tenantName) {
-                context += ` for tenant '${deploymentResult?.tenantName}'`;
-            }
+            if (deploymentResult) {
+                if (deploymentResult?.environmentName) {
+                    context = ` to environment '${deploymentResult.environmentName}'`;
+                }
+                if (deploymentResult?.tenantName) {
+                    context += ` for tenant '${deploymentResult?.tenantName}'`;
+                }
 
-            if (t.IsCompleted) {
-                this.logger.info?.(`Deployment${context} ${t.State === TaskState.Success ? "completed successfully" : "did not complete successfully"}`);
-            } else {
-                this.logger.info?.(`Deployment${context} is '${t.State}'`);
-            }
+                if (t.IsCompleted) {
+                    this.logger.info?.(`Deployment${context} ${t.State === TaskState.Success ? "completed successfully" : "did not complete successfully"}`);
+                } else {
+                    this.logger.info?.(`Deployment${context} is '${t.State}'`);
+                }
 
-            if (t.IsCompleted && t.State != TaskState.Success) {
-                failedTaskIds.push(t.Id);
+                if (t.IsCompleted) {
+                    deploymentResult.successful = t.IsCompleted && t.State == TaskState.Success;
+                    deploymentResults.push(deploymentResult);
+                }
             }
         });
 
-        if (failedTaskIds.length > 0) {
-            this.task.setFailure(`${failedTaskIds.length} ${failedTaskIds.length === 1 ? "task" : "tasks"} failed.`);
+        const spaceId = await this.getSpaceId(client, inputParameters.space);
+        let failedDeploymentsCount = 0;
+        deploymentResults.map((r) => {
+            const url = `${this.connection.url}app#/${spaceId}/tasks/${r.serverTaskId}`;
+            const context = this.getContext(r);
+            if (r.successful) {
+                this.logger.info?.(`Succeeded: ${url}`);
+            } else {
+                this.logger.warn?.(`Failed: ${url}`);
+                failedDeploymentsCount++;
+            }
+            this.task.setOutputVariable(`${context}.completed_successfully`, r.successful.toString());
+        });
+
+        if (failedDeploymentsCount > 0) {
+            this.task.setFailure(`${failedDeploymentsCount} ${failedDeploymentsCount == 1 ? "task" : "tasks"} failed.`);
+            this.task.setOutputVariable("completed_successfully", "false");
+        } else {
+            this.task.setSuccess("All tasks completed successfully");
+            this.task.setOutputVariable("completed_successfully", "true");
         }
+
+        this.task.setOutputVariable("server_task_results", JSON.stringify(deploymentResults));
+    }
+
+    async getSpaceId(client: Client, spaceName: string): Promise<string | undefined> {
+        const spaceRepository = new SpaceRepository(client);
+        const spaceList = await spaceRepository.list({ partialName: spaceName });
+        const matches = spaceList.Items.filter(s => s.Name.localeCompare(spaceName) === 0);
+        return matches.length > 0 ? matches[0].Id : undefined;
+    }
+
+    getContext(result: DeploymentResult): string {
+        return result.tenantName ? result.tenantName.replace(" ", "_") : result.environmentName.replace(" ", "_");
     }
 }
