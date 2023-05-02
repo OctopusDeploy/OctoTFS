027c14e4fa8887438b4479c5e9344ace9e4bda1e
Ben Pearce <ben.pearce@octopus.com>
2023-05-02 15:44:41 +1000

    wip

--- source/tasks/DeployTenant/TenantedDeployV6/deploy.ts
+++ source/tasks/DeployTenant/TenantedDeployV6/deploy.ts
@@ -1,10 +1,11 @@
-import { Logger } from "@octopusdeploy/api-client";
+import { CreateDeploymentTenantedCommandV1, Logger, Client, resolveSpaceId, EnvironmentRepository } from "@octopusdeploy/api-client";
 import { OctoServerConnectionDetails } from "../../Utils/connection";
 import { createDeploymentFromInputs } from "./createDeployment";
 import { createCommandFromInputs } from "./inputCommandBuilder";
 import os from "os";
 import { TaskWrapper } from "tasks/Utils/taskInput";
 import { getClient } from "../../Utils/client";
+import { ExecutionResult } from "../../Utils/executionResult";
 
 export class Deploy {
     constructor(readonly connection: OctoServerConnectionDetails, readonly task: TaskWrapper, readonly logger: Logger) {}
@@ -14,7 +15,9 @@ export class Deploy {
             const command = createCommandFromInputs(this.logger, this.task);
             const client = await getClient(this.connection, this.logger, "release", "deploy-tenanted", 6);
 
-            createDeploymentFromInputs(client, command, this.task, this.logger);
+            const results = await createDeploymentFromInputs(client, command, this.task, this.logger);
+
+            await this.tryCreateSummary(client, command, results);
 
             this.task.setSuccess("Deployment succeeded.");
         } catch (error: unknown) {
@@ -25,5 +28,12 @@ export class Deploy {
             }
             throw error;
         }
+    }
+
+    private async tryCreateSummary(client: Client, command: CreateDeploymentTenantedCommandV1, results: ExecutionResult[]) {
+        const spaceId = await resolveSpaceId(client, command.spaceName);
+        const environmentRepo = new EnvironmentRepository(client, command.spaceName);
+        environmentRepo
+
     }
 }
